1042c054a0d2b37ba23bab229dbcbbffa4a37e69
Paul Maximilian Krüger <paul.krueger@alumni.fh-aachen.de>
2021-03-03 17:09:51 +0100

    Updated run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Config
 GITHUB_REPO="adclock/server"
 JAR_FILE="server.jar"
-DELAY=5
+DELAY=5 # seconds
 
 
 # Functions
@@ -18,17 +18,17 @@ while :
 do
     # Checkout latest release
     LATEST_RELEASE=$(get_latest_release $GITHUB_REPO)
-    echo "Latest release is $LATEST_RELEASE" 
+    echo "Latest release of $GITHUB_REPO is $LATEST_RELEASE" 
 
     if [ ! -d "./$LATEST_RELEASE" ]; then
-      echo "New release $LATEST_RELEASE. Creating subfolder ..."
+      echo "New release $LATEST_RELEASE found. Creating subfolder ..."
       mkdir "./$LATEST_RELEASE"
     fi 
 
-    cd "./$LATEST_RELEASE"
+    cd "./$LATEST_RELEASE" # enter release folder
 
     if [ ! -f "./$JAR_FILE" ]; then
-      echo "Jar $JAR_FILE not found in release. Try to download ..."
+      echo "Jar not found. Download $JAR_FILE @ $LATEST_RELEASE from GitHub ..."
       curl --silent -O -J -L "https://github.com/$GITHUB_REPO/releases/download/$LATEST_RELEASE/$JAR_FILE"
     fi 
 
@@ -37,11 +37,11 @@ do
     rc=$?
     echo "$JAR_FILE returned $rc."
 
-    if [[ $rc == 1 ]]; then
-        echo "An error occurred. Waiting $DELAY seconds..."
-        sleep $DELAY
+    if [[ "$rc" == 1 ]]; then
+        echo "An error occurred. Waiting $DELAY seconds ..."
+        sleep $DELAY # seconds
     fi
     
-    echo "Try to restart... "
-    cd .. # Leave release folder
+    cd .. # leave release folder
+    echo "Try to restart $JAR_FILE ... "
 done
